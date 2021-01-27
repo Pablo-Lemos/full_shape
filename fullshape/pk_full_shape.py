a87dcd555cc413400d6d30d2c54cda2c2a5d34e0
Pablo Lemos <plemos91@gmail.com>
2021-01-27 15:36:39 +0000

    Implemented anisotropic BAO damping

--- fullshape/pk_full_shape.py
+++ fullshape/pk_full_shape.py
@@ -1,56 +1,8 @@
-''' This module calculates the anisotropic power spectrum
-
-To do:
-- add BAO damping
-- calculate covariance matrix (diff module)
-- add noise (diff module)
-- improve integration (w convergence criterion)
-'''
-
-
 import numpy as np
-from scipy.integrate import simps 
 import camb
-
-def get_growth_factor(results):
-    '''returns the growth factor from a CAMB results object'''
-    return results.get_fsigma8()/results.get_sigma8()[0]
-
-def generate_power_spectrum(zs = [0.], minkh=5e-5, maxkh = 1, npoints = 200, 
-                            As=2.142e-9, ns=0.9667, H0=67.36, ombh2=0.02230, 
-                            omch2=0.1188, mnu=0.06, omk=0, tau=0.06):
-
-    ''' 
-    Given redshift, a min and max kh, a number of k bins and cosmological 
-    parameters, returns a kh array and the matter power spectrum from CAMB. 
-    It also returns the growth factor f and the redshifts (as they might be
-    resorted).
-    '''
-
-    pars = camb.CAMBparams()
-    pars.set_cosmology(H0=H0, ombh2=ombh2, omch2=omch2, mnu=mnu, omk=omk, 
-                       tau=tau)
-    pars.InitPower.set_params(As=As, ns=ns, r=0)
-
-    pars.set_matter_power(redshifts=zs, kmax=2.0)
-    pars.NonLinear = camb.model.NonLinear_none
-    results = camb.get_results(pars)
-    f = get_growth_factor(results)
-    kh, zs, pk = results.get_matter_power_spectrum(minkh=minkh, maxkh=maxkh, 
-                                                  npoints = npoints)
-
-    return kh, zs, pk, f
-
-def get_kaiser_factor(mu,b1,f):
-    ''' Calculate the Kaiser factor'''
-    kaiser = (b1+np.outer(f,mu**2))**2.
-    return kaiser[:,np.newaxis, :]
-
-def get_fog_factor(kh, mu, sigma_v, f):
-    ''' Calculate the Fingers of God factor'''
-    temp = sigma_v*np.outer(kh, mu)
-    logf = np.einsum('i, jk -> ijk', f, temp)
-    return np.exp(-(logf)**2.)
+from scipy.integrate import simps 
+import sys
+from smooth_pk import minimize_smooth_pk
 
 def get_legendre_2(x):
     ''' Returns the second order Legendre polynomial'''
@@ -60,19 +12,82 @@ def get_legendre_4(x):
     ''' Returns the fourth order Legendre polynomial'''
     return (1/8.)*(35.*x**4-30*x**2.+3)
 
-def get_anisotropic_pk(kh, pk, f, b1, sigma_v):
-    ''' Returns anisotropic power spectra, with dimensions [redshift, k]'''
-    mu_arr = np.linspace(0,1,1000)
-    kaiser_arr = get_kaiser_factor(mu_arr, b1, f)
-    fog_arr = get_fog_factor(kh, mu_arr, sigma_v, f)
-    Pg0 = fog_arr*kaiser_arr*pk[:,:,np.newaxis]
-    Pg2 = Pg0*get_legendre_2(mu_arr)
-    Pg4 = Pg0*get_legendre_4(mu_arr)
-    P_theory_0 = simps(y = Pg0, x = mu_arr, axis = -1)
-    P_theory_2 = simps(y = Pg2, x = mu_arr, axis = -1)
-    P_theory_4 = simps(y = Pg4, x = mu_arr, axis = -1)
- 
-    #P_theory_0 = np.trapz(Pg0, mu_arr, axis = -1)
-    #P_theory_2 = np.trapz(Pg2, mu_arr, axis = -1)
-    #P_theory_4 = np.trapz(Pg4, mu_arr, axis = -1)
-    return P_theory_0, P_theory_2, P_theory_4
+class PK_Calculator:
+    def __init__(self, zs = [0.], minkh=5e-5, maxkh = 1, num_k = 200, num_mu = 1000, 
+                            As=2.142e-9, ns=0.9667, H0=67.36, ombh2=0.02230, 
+                            omch2=0.1188, mnu=0.06, omk=0, tau=0.06):
+        ''' The initial function generates the camb pk
+        
+        The arrays have dimensions [z, k, mu]
+        '''
+
+        pars = camb.CAMBparams()
+        pars.set_cosmology(H0=H0, ombh2=ombh2, omch2=omch2, mnu=mnu, omk=omk, 
+                        tau=tau)
+        pars.InitPower.set_params(As=As, ns=ns, r=0)
+
+        pars.set_matter_power(redshifts=zs, kmax=2.0)
+        pars.NonLinear = camb.model.NonLinear_none
+        self.results = camb.get_results(pars)
+        self.f = self.results.get_fsigma8()/self.results.get_sigma8()[0]
+        self.kh, self.zs, pk = self.results.get_matter_power_spectrum(
+                             minkh=minkh, maxkh=maxkh, npoints = num_k)
+        
+        self.mu = np.linspace(0, 1, num_mu)
+        # Reshape pk into [z,k,mu] shape
+        self.pk_camb = pk[:,:,np.newaxis]
+
+
+    def kaiser_factor(self, b1):
+        ''' Calculate the Kaiser factor'''
+        kaiser = (b1+np.outer(self.f,self.mu**2))**2.
+        return kaiser[:,np.newaxis, :]
+
+    def fog_factor(self, sigma_v):
+        ''' Calculate the Fingers of God factor'''
+        temp = sigma_v*np.outer(self.kh, self.mu)
+        logfog = np.einsum('i, jk -> ijk', self.f, temp)
+        return np.exp(-(logfog)**2.)
+
+    def calculate_BAO_damping(self, sigma_per, sigma_par, b1):
+        mu = np.reshape(self.mu, [1,1,-1])
+        k = np.reshape(self.kh, [1,-1,1])
+        logdamp = k**2/2*(mu**2*sigma_par**2 + (1 - mu**2)*sigma_per**2)
+        return np.exp(-logdamp)
+
+    def add_BAO_damping(self, sigma_per, sigma_par, b1):
+        ps = np.empty([len(self.zs), len(self.kh), 1])
+        for (i,pk) in enumerate(self.pk_camb[:,:,0]):
+            ps[i, :, 0] = minimize_smooth_pk(self.kh, pk)
+        pnl = self.pk_camb - ps
+        bao_damp_factor = self.calculate_BAO_damping(sigma_per, sigma_par, b1)
+        pnl = np.einsum('ijk, ijk -> ijk', pnl, bao_damp_factor)
+        pnl *= bao_damp_factor
+        return pnl + ps
+
+    def get_anisotropic_pk(self, sigma_per, sigma_par, b1, sigma_v, integration = 'Simps'):
+        ''' Returns anisotropic power spectra, with dimensions [redshift, k]'''
+
+        kaiser = self.kaiser_factor(b1)
+        fog = self.fog_factor(sigma_v)
+        
+        Pmu0 = self.add_BAO_damping(sigma_per, sigma_par, b1)
+        Pmu0 = fog*kaiser*Pmu0
+        Pmu2 = Pmu0*get_legendre_2(self.mu)
+        Pmu4 = Pmu0*get_legendre_4(self.mu)
+
+        if integration == 'Simps':
+            self.p0 = simps(y = Pmu0, x = self.mu, axis = -1)
+            self.p2 = simps(y = Pmu2, x = self.mu, axis = -1)
+            self.p4 = simps(y = Pmu4, x = self.mu, axis = -1)
+        
+        elif integration == 'Trapz':
+            self.p0 = np.trapz(y = Pmu0, x = self.mu, axis = -1)
+            self.p2 = np.trapz(y = Pmu2, x = self.mu, axis = -1)
+            self.p4 = np.trapz(y = Pmu4, x = self.mu, axis = -1)
+
+        else:
+            print("Integration must be either 'Simps' or 'Trapz' ")
+            raise
+
+        
---
+++ fullshape/smooth_pk.py
@@ -0,0 +1,29 @@
+'''
+This module calculates the anisotropic BAO damping for a given power spectrum
+
+Based on Lado's Julia script.
+'''
+
+import numpy as np
+import scipy.optimize as optimize
+
+def get_smooth_pk(k, pars):
+    keq, A, a0, a2, a4 = pars
+    q = k/keq
+    L = np.log(2*np.exp(1) + 1.8*q)
+    C = 14.2 + 731.0/(1 + 62.5*q)
+    T = L/(L + C*q**2)
+    return A*(T**2 *k + a0 + a2*k**2 + a4*k**4)
+
+def get_chisq_smooth_pk(k, pk, pars):
+    smooth_p = get_smooth_pk(k, pars)
+    chi2 = np.sum(k**2*(pk - smooth_p)**2)
+    return chi2
+
+def minimize_smooth_pk(k, pk):
+    chi2 = lambda params: get_chisq_smooth_pk(k, pk, params) 
+    initial_guess = [0.6, 60000, 0.1, 0., 0.]
+    result = optimize.minimize(chi2, initial_guess)
+    best_pars = result.x
+    smooth_pk = get_smooth_pk(k, best_pars)
+    return smooth_pk
